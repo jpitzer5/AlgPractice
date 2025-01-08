dfbbe28b812bc35885a5aa8115db223044f36bae
Joel Pitzer <jpitzer5@gmail.com>
2025-01-08 11:04:38 -0800

    copied outline for all chapters

---
+++ CrackingTheCodingInterview/ch13/algs.py
@@ -0,0 +1,46 @@
+### Chapter 1: Arrays and Strings
+
+#1.1 Determine if a string has all unique chars
+def CH1_1():
+    pass
+
+#1.1a What if we can't use add'l data structures
+def CH1_1b():
+    pass
+
+# 1.2 Given 2 strings, determine if one is a permutation of the other
+def CH1_2():
+    pass
+
+#1.3 Replace all spaces in a string with "%20" (input: string with addl padding at end to make space for addl chars, true length of string)
+def CH1_3():
+    pass
+
+#1.4 Given a string, check if it is a permutation of a palindrome
+def CH1_4():
+    pass
+
+#1.5 Given 2 strings check if they are 0 or 1 edits away (edit = insertion, deletion, replacement of a char)
+def CH1_5():
+    pass
+
+#1.6 Compress strings like aabcccccaaa -> a2b1c5a3 (if compressed string isn't smaller, just return original)
+def CH1_6():
+    pass
+
+#1.7 Given an NxN matrix of ints, rotate it 90 deg
+def CH1_7():
+    pass
+
+#1.7a Can you do it in place?
+def CH1_7a():
+    pass
+
+#1.8 Given an NxN matrix, if an element is 0 set its entire row and column to 0
+def CH1_8():
+    pass
+
+#1.9 Assume you have a function isSubstring which checks if one word is a substring of the other. Given two string, check if the second is a rotation of the first
+# (waterbottle is a rotation of erbottlewat)
+def CH1_9():
+    pass
---
+++ CrackingTheCodingInterview/ch3/test.py
@@ -0,0 +1,141 @@
+import unittest
+from algs import CH1_1, CH1_2, CH1_3, CH1_4, CH1_5, CH1_6, CH1_7, CH1_8, CH1_9
+
+
+class Test1_1(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_1()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_1()
+
+        self.assertEqual(result, expected)
+
+class Test1_2(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_2()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_2()
+
+        self.assertEqual(result, expected)
+
+class Test1_3(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_3()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_3()
+
+        self.assertEqual(result, expected)
+
+class Test1_4(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_4()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_4()
+
+        self.assertEqual(result, expected)
+
+class Test1_5(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_5()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_5()
+
+        self.assertEqual(result, expected)
+
+class Test1_6(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_6()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_6()
+
+        self.assertEqual(result, expected)
+
+class Test1_7(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_7()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_7()
+
+        self.assertEqual(result, expected)
+
+class Test1_8(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_8()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_8()
+
+        self.assertEqual(result, expected)
+
+class Test1_9(unittest.TestCase):
+    def test_base_case_1(self):
+        expected = 0
+
+        result = CH1_9()
+
+        self.assertEqual(result, expected)
+
+    def test_case_1(self):
+        expected = 0
+
+        result = CH1_9()
+
+        self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main()
